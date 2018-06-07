e9cd3d0ab91db1d1be0440c800f98863104f4ab9
Achraf OUBRAHIM <achraf.oubrahim@gmail.com>
2018-06-07 10:31:32 +0200

    commit final

---
+++ inc/connexion.inc.php
@@ -0,0 +1,8 @@
+<?php /* fichier cnam/nfa017/2017-cf1/inc/connexion.php - 20180604-PBO */
+
+function connexion($bddname) {
+ $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+ $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND]= 'SET NAMES utf8';
+ $con = new PDO('mysql:host=sql210.epizy.com;dbname=epiz_22177148_cnamcp09_nfa083cf1_2017','epiz_22177148', 'Mdpcontrole083', $pdo_options);
+ return $con;
+}
---
+++ index.php
@@ -0,0 +1,34 @@
+<!-- fichier cnam/nfa017/2017-cf1/index.inc.php - 20180604-PBO -->
+
+<!-- ENTETE HTML =============================================================== --><?php
+     include("inc/entete.inc.php");
+     entete("NFA083 CF1 : Présentation hébergeur",0); /* pour le title et le h1 */ ?>
+
+<!-- PRESENTATION Hébergeur ============================================================== -->
+  <section><h2>Informations sur mon hébergeur</h2>
+           <p><strong>Nom : </strong> InfinityFree</p>
+           <p><strong>Logo : </strong> <img src="img/infinityfree.jpg" /></p>
+           <p><strong>Siège social : </strong> Bulman House, Regent Centre, Gosforth, Newcastle upon Tyne, NE3 3LS</p>
+           <p><strong>Lien d'inscription : </strong>
+           <a href="https://infinityfree.net/" target="_blanck" >Inscription InfinityFree</a></p>
+  </section>
+  <section><h2>Informations sur la page question.php</h2>
+           <p><strong>Temps de chargement total en ms( https://tools.pingdom.com/ ): </strong> 442 ms</p>
+           <p><strong>Temps lié hébergeur (ms) : </strong> 	176 ms</p>
+           <p><strong>Temps lié image (ms) : </strong> 88 ms</p>
+           <p><strong>Temps lié CSS (ms) : </strong> 88 ms</p>
+           <p><strong>Temps lié JS (ms) : </strong> 88 ms</p>
+           <p><strong>Adresse IP d'hébergement de la page question.php 
+           (http://www.mon-ip.com/adresse-ip-site-internet.php) : </strong> 185.27.134.218</p>
+           <p><strong>Nom du serveur d'hébergement de la page question.php : </strong> ns1.byet.org</p>
+  </section>
+  <section><h2>Complément d'information</h2>
+           <p><strong>Une question complémentaire ? </strong> 
+              <a href="question.php" id="question" class="actif">Poser une question</a>
+           </p>
+  </section>
+  
+  <footer>Copyright A.OUBRAHIM</footer>
+
+</body>
+</html>
